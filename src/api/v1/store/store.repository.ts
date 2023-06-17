8578768f1d9829f66854f1c111af8785072eb764
woomae <cwg0809@naver.com>
2023-06-17 17:11:34 +0900

    지역필터링을 위한 API수정
    - api/v1/store/안에 search메소드에 filter 파라미터 추가
    - filter 파라미터 존재유무에 따라 검색방식 변경

--- src/api/v1/store/store.repository.ts
+++ src/api/v1/store/store.repository.ts
@@ -1,30 +1,12 @@
-import { Op } from 'sequelize';
-import { Store } from '../../../models/store.model';
+import { Op } from "sequelize";
+import { Store } from "../../../models/store.model";
 
 export default class StoreRepository {
-	/**
+    /**
      주어진 조건에 맞는 Record 모두 검색
      */
-	 public findAll(page, size): Promise<{ rows; count }> {
+    public findAll(page, size): Promise<{ rows; count }> {
         let where = {
-                deleted_at: {
-                    [Op.eq]: null,
-                },
-        };
-
-        return Store.findAndCountAll({
-            where,
-            limit: size,
-            offset: size * (page - 1),
-            order: [['_id', 'ASC']],
-        });
-    }
-
-    public search(q, page, size): Promise<{ rows; count }> {
-        let where = {
-            name: {
-                [Op.like]: `%${q}%`,
-            },
             deleted_at: {
                 [Op.eq]: null,
             },
@@ -34,7 +16,40 @@ export default class StoreRepository {
             where,
             limit: size,
             offset: size * (page - 1),
-            order: [['_id', 'ASC']],
+            order: [["_id", "ASC"]],
+        });
+    }
+
+    public search(q, page, size, filter): Promise<{ rows; count }> {
+        let where;
+        if (filter !== undefined) {
+            where = {
+                name: {
+                    [Op.like]: `%${q}%`,
+                },
+                deleted_at: {
+                    [Op.eq]: null,
+                },
+                address: {
+                    [Op.like]: `%${filter}%`,
+                },
+            };
+        } else {
+            where = {
+                name: {
+                    [Op.like]: `%${q}%`,
+                },
+                deleted_at: {
+                    [Op.eq]: null,
+                },
+            };
+        }
+
+        return Store.findAndCountAll({
+            where,
+            limit: size,
+            offset: size * (page - 1),
+            order: [["_id", "ASC"]],
         });
     }
 }
--- src/api/v1/store/store.service.ts
+++ src/api/v1/store/store.service.ts
@@ -1,34 +1,45 @@
-import { Store } from '../../../models/store.model';
-import StoreRepository from './store.repository';
-import sequelize from 'sequelize';
-import { assertNotNull } from '../../../lib/utils';
-import ApiError from '../../../common/api.error';
-import ApiCodes from '../../../common/api.codes';
-import ApiMessages from '../../../common/api.messages';
+import { Store } from "../../../models/store.model";
+import StoreRepository from "./store.repository";
+import sequelize from "sequelize";
+import { assertNotNull } from "../../../lib/utils";
+import ApiError from "../../../common/api.error";
+import ApiCodes from "../../../common/api.codes";
+import ApiMessages from "../../../common/api.messages";
 
 export default class StoreService {
     list = async (page, size): Promise<{ rows; count }> => {
         return new StoreRepository().findAll(page, size);
     };
 
-    search = async (q, page, size): Promise<{ rows; count }> => {
-        return new StoreRepository().search(q, page, size);
-    }
+    search = async (q, page, size, filter): Promise<{ rows; count }> => {
+        return new StoreRepository().search(q, page, size, filter);
+    };
 
-    nearbyList = async ({ longitude, latitude, distance }): Promise<{ rows; count }> => {
-        assertNotNull(latitude && longitude, new ApiError(ApiCodes.BAD_REQUEST, ApiMessages.BAD_REQUEST, {
-            message: 'latitude and longitude are required'
-        }));
+    nearbyList = async ({
+        longitude,
+        latitude,
+        distance,
+    }): Promise<{ rows; count }> => {
+        assertNotNull(
+            latitude && longitude,
+            new ApiError(ApiCodes.BAD_REQUEST, ApiMessages.BAD_REQUEST, {
+                message: "latitude and longitude are required",
+            })
+        );
 
         const stores = await Store.findAndCountAll({
             where: sequelize.where(
-              sequelize.literal(`(6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) * 1000`),
-              '<=',
-              distance
+                sequelize.literal(
+                    `(6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) * 1000`
+                ),
+                "<=",
+                distance
             ),
-            order: sequelize.literal(`(6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) * 1000`)
-          });
+            order: sequelize.literal(
+                `(6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) * 1000`
+            ),
+        });
 
-          return stores;
-    }
+        return stores;
+    };
 }
