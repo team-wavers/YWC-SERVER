37c534d33739858c5ce61050cd8e6bdd8e8d67ca
다운 <j_d_woon@naver.com>
2023-11-24 16:45:28 +0900

    refactor: 쿠키 검증 미들웨어
    - api 규격 통일
    - 옵셔널 체이닝 적용

--- src/api/v1/auth/auth.router.ts
+++ src/api/v1/auth/auth.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import AuthController from "./auth.controller";
+import verifyCookie from "../../../lib/verify-cookie";
 
 export const path = "/auth";
 export const router = Router();
 
 router.post("/signin", new AuthController().adminSignIn);
-router.post("/signout", new AuthController().adminSignOut);
+router.post("/signout", verifyCookie, new AuthController().adminSignOut);
--- src/lib/verify-cookie.ts
+++ src/lib/verify-cookie.ts
@@ -1,30 +1,36 @@
 import { Result } from "../../src/common/result";
 import logger from "./logger";
 import httpStatus from "http-status";
+import ApiError from "src/common/api.error";
+import ApiCodes from "src/common/api.codes";
+import ApiMessages from "src/common/api.messages";
 
-function verifyCookie(req, res, next) {
-    if (!req.cookies.admin) {
-        return handleAuthError(res, "Admin cookie not found");
+export default function verifyCookie(req, res, next) {
+    try {
+        const token = req.cookies.admin?.role;
+
+        if (!token) {
+            throw new ApiError(ApiCodes.OK, ApiMessages.NOT_FOUND, {
+                message: "Token not found",
+            });
+        }
+
+        if (!isTokenValid(token)) {
+            throw new ApiError(ApiCodes.OK, ApiMessages.UNAUTHORIZED, {
+                message: "Invalid token",
+            });
+        }
+
+        next();
+    } catch (e) {
+        logger.err(JSON.stringify(e));
+        logger.error(e);
+
+        const response = Result.fail<Error>(e).toJson();
+        res.status(httpStatus.OK).json(response);
     }
-
-    const { token } = req.cookies.admin;
-
-    if (!token) {
-        return handleAuthError(res, "Token not found");
-    }
-
-    if (token.role !== "admin") {
-        return handleAuthError(res, "Invalid token");
-    }
-    next();
 }
 
-function handleAuthError(res, message) {
-    const error = new Error(message);
-    const response = Result.fail<Error>(error).toJson();
-
-    logger.err(JSON.stringify(error));
-    logger.error(error);
-
-    res.status(httpStatus.UNAUTHORIZED).json(response);
+function isTokenValid(token) {
+    return token === "admin";
 }
